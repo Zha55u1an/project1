fd05ecb33c57919172cd26e26d073180ad0d833d
Zha55u1an <120386605+Zha55u1an@users.noreply.github.com>
2025-01-17 00:26:29 +0500

    Update dbInfo.go

--- pkg/db/dbInfo.go
+++ pkg/db/dbInfo.go
@@ -1,11 +1,5 @@
 package db
 
 const (
-	Host     = "localhost"
-	Port     = 5432
-	Username = "postgres"
-	Password = "Zhasulan2004"
-	Dbname   = "ucms"
-	TokenKey = "servertoken"
-	SSLMode  = "disable"
+	
 )
